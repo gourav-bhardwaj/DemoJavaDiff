2dba7d386f51fef840f68e9330db32286c6f47a3
Gourav <gov.kumarbharatdwaj@gmail.com>
2020-08-29 13:52:36 +0530

    Main class changes

--- src/com/demo/Main.java
+++ src/com/demo/Main.java
@@ -2,6 +2,6 @@ package com.demo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Joker mila h mujko!!");
+        System.out.println("Hello world");
     }
 }

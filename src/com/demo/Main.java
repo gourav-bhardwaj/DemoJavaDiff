ef80645b4a4e2ee5ed714b0b5f68fd9c12fd038e
Gourav <gov.kumarbharatdwaj@gmail.com>
2020-08-29 12:10:27 +0530

    revert last chnages.

--- src/com/demo/Main.java
+++ src/com/demo/Main.java
@@ -2,7 +2,6 @@ package com.demo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is daliy lifetime work by me!!??");
-        System.out.println("Again come......hurry");
+
     }
 }
